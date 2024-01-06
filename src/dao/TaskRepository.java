b540a1de252f113cc2b1f710871c937a33477224
dhulipalasrikanth98 <dhulipalasrikanth98@outlook.com>
2024-01-06 20:11:01 +0530

    completed the basic scenarios handling  of update and delete task

--- src/dao/TaskRepository.java
+++ src/dao/TaskRepository.java
@@ -1,7 +1,7 @@
 package dao;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
 
 import dto.Task;
 
@@ -13,11 +13,37 @@ public class TaskRepository {
         return taskDB.add(task);
     }
 
+    public boolean updateTask(Task updatedTask){
+        OptionalInt optionalInt = findTask(updatedTask);
+       if(optionalInt.isPresent() && checkForChanges(updatedTask,taskDB.get(optionalInt.getAsInt()))){
+               taskDB.set(optionalInt.getAsInt(),updatedTask);
+       }
+        return false;
+    }
+
+    private boolean checkForChanges(Task updatedTask,Task task) {
+        return !updatedTask.getTaskName().equals(task.getTaskName()) || !updatedTask.getTaskDeadline().equals(task.getTaskDeadline()) || !updatedTask.getTaskStatus().equals(task.getTaskStatus());
+    }
+
+    public boolean delete(Task updatedTask){
+        OptionalInt optionalInt = findTask(updatedTask);
+        if(optionalInt.isPresent() && checkForChanges(updatedTask,taskDB.get(optionalInt.getAsInt()))){
+            taskDB.remove(optionalInt.getAsInt());
+        }
+        return false;
+    }
+
     public List<Task> getTasks() {
         return taskDB;
     }
 
+    private OptionalInt findTask(Task updatedTask) {
 
+        return IntStream.range(0, taskDB.size())
+                .filter(i -> Objects.equals(taskDB.get(i).getTaskId(), updatedTask.getTaskId()))
+                .findFirst();
+
+    }
     
     
 }
